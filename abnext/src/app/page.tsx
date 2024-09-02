2b693aed480aea91b6cf9a7a5b09e92fa3abe975
Mateusz Dziurkiewicz <mateusz+git@plsr.uk>
2024-09-02 20:16:42 +0100

    chore(home): Simplified home page. Just shows button to archives page since that handles all the auth stuff anyway

--- abnext/src/app/page.tsx
+++ abnext/src/app/page.tsx
@@ -1,23 +1,12 @@
-import { cookies } from 'next/headers'
 import Link from 'next/link'
 
 /**
- * Home page of the app. Redirects to the archives page if the user is signed in, otherwise
- * shows a sign in button.
+ * Home page of the app.
  */
 export default async function Home() {
-  const session = cookies().get('session')
-
   return (
-    <>
-      {session ? (
-        <Link href={'/archives'}>View archives</Link>
-      ) : (
-        <Link href={process.env.oAuth_url as string}>
-          <button>Sign In</button>
-        </Link>
-      )}
-    </>
+    <Link href={'/archives'}>
+      <button>View Archives</button>
+    </Link>
   )
 }
-
